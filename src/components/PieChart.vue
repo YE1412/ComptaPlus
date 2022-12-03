1131d7848bbe14341d880c0d4f9b4f9579fa7804
YE1412 <yannick.elusue@hotmail.fr>
2022-12-03 16:30:25 +0100

    update home stats pie chart

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -9,6 +9,7 @@ import {
   ArcElement,
   CategoryScale,
 } from 'chart.js';
+import { useCounterStore } from "@/stores/counter";
 
 ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale)
 
@@ -46,14 +47,31 @@ export default defineComponent({
 		Pie,
 	},
 	setup(props) {
-	    const chartData = {
-	      labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
-	      datasets: [
-	        {
-	          backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
-	          data: [40, 20, 80, 10]
-	        }
-	      ]
+		const cntStore = useCounterStore();
+	    
+	    return {
+	    	counterStore: cntStore,
+	    };
+	},
+	data() {
+		const payStats = this.getPaymentsStats();
+		const cbVal = ((payStats.cb * 100)/payStats.all).toFixed(0);
+		const cashVal = ((payStats.cash * 100)/payStats.all).toFixed(0);
+		const chqVal = ((payStats.chq * 100)/payStats.all).toFixed(0);
+		const chartData = {
+			labels: [
+		      	this.$i18n.t("cbLabel"),
+		      	this.$i18n.t("cashLabel"),
+		      	this.$i18n.t("chqLabel"),
+	    	],
+			datasets: [
+		        {
+		          // backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+		          // data: [40, 20, 80, 10]
+					backgroundColor: ['#41B883', '#E46651', '#00D8FF'],
+					data: [cbVal, cashVal, chqVal]
+		        }
+	      	]
 	    };
 
 	    const chartOptions = {
@@ -64,17 +82,61 @@ export default defineComponent({
 	    return {
 	        chartData,
 	        chartOptions,
-	        chartId: props.chartId,
-	        width: props.width,
-	        height: props.height,
-	        cssClasses: props.cssClasses,
-	        styles: props.styles,
-	        plugins: props.plugins,
-	        datasetIdKey: props.datasetIdKey,
+	        // chartId: props.chartId,
+	        // width: props.width,
+	        // height: props.height,
+	        // cssClasses: props.cssClasses,
+	        // styles: props.styles,
+	        // plugins: props.plugins,
+	        // datasetIdKey: props.datasetIdKey,
 		};
+	},
+	methods: {
+		getPaymentsStats() {
+			let ret = {cb: 0, cash: 0, chq: 0, all: 0};
+			for (const k in this.counterStore.getInvoicesFY) {
+				for (const l in this.counterStore.getInvoicesFY[k]["payments"]) {
+					if (this.counterStore.getInvoicesFY[k]["payments"][l].etat)
+					{
+						switch(this.counterStore.getInvoicesFY[k]["payments"][l].paymentType) {
+							case 1:
+								ret.cb += 1;
+								ret.all += 1;
+								break;
+							case 2:
+								ret.cash += 1;
+								ret.all += 1;
+								break;
+							case 3:
+								ret.chq += 1;
+								ret.all += 1;
+								break;
+							default:
+								break;
+						}
+					}
+				}
+			}
+			return ret;
+		}
 	}
 });
 </script>
+
+<i18n>
+{
+	"fr": {
+		"cbLabel": "Carte bleue",
+		"cashLabel": "Espèces",
+		"chqLabel": "Chèques",
+	},
+	"en": {
+		"cbLabel": "Credit card",
+		"cashLabel": "Cash",
+		"chqLabel": "Check",
+	}
+}
+</i18n>
 
 <template>
 	<Pie
